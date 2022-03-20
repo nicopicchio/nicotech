46e89bb913b0b8f95bbe40414e311612f1d8bb61
Nico Picchio <nicolapicchio@gmail.com>
2022-03-20 13:53:20 +0000

    about me section and form behaviour added

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,5 +1,32 @@
+import profileImg from '../../images/profile-photo.jpg';
+
 function About() {
-	return <p>About me section</p>;
+	return (
+		<div id='about-section'>
+			<div className='about-me-container'>
+				<h3 className='about-me-title'>About me</h3>
+				<p className='about-me-text'>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sed
+					finibus lacus. Praesent elementum, tellus et accumsan malesuada, urna
+					tortor luctus libero, in molestie urna nulla ac mauris. Nulla vel
+					pharetra massa, in faucibus lectus. Cras quis condimentum ligula, vel
+					pharetra lorem. In lacus ipsum, varius et erat sed, viverra varius
+					sapien. Sed in feugiat dui. Ut congue erat id nisi dapibus aliquam
+					vitae eget mauris. In hac habitasse platea dictumst. Vestibulum in
+					bibendum est. Vivamus sit amet mauris ac enim ultrices suscipit vel
+					eget risus.
+				</p>
+			</div>
+			<div className='profile-image-container'>
+				<img
+					src={profileImg}
+					alt='profile photo'
+					className='profile-image'
+					width='510px'
+				/>
+			</div>
+		</div>
+	);
 }
 
 export default About;
--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,21 +1,21 @@
 function Contact() {
 	return (
-		<form id='contact-form'>
-			<h2 id='contact-form-title'>Contact me!</h2>
+		<form id='contact-form' action='https://formsubmit.co/nico@nicotech.dev' method='POST'>
+			<h2 className='contact-form-title'>Contact me!</h2>
 			<label className='form-label'>
 				Name<span className='required-field'>*</span>
 			</label>
-			<input type='text' required />
+			<input type='text' name='name' required />
 			<label className='form-label'>
 				Email<span className='required-field'>*</span>
 			</label>
-			<input type='email' required />
+			<input type='email' name='email' required />
 			<label className='form-label'>Phone number</label>
 			<input type='text' />
 			<label className='form-label'>
 				Enquiry<span className='required-field'>*</span>
 			</label>
-			<textarea type='text' name='enquiry' cols='40' rows='10' required />
+			<textarea type='text' name='message' cols='40' rows='10' required />
 			<div id='contact-form-button'>
 				<button type='submit' value='submit'>
 					Submit
