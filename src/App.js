4d34240ac202aee1db6da5fab486afba06234bac
Nico Picchio <nicolapicchio@gmail.com>
2022-04-04 12:13:26 +0100

    react router update

--- src/App.js
+++ src/App.js
@@ -8,18 +8,16 @@ import { Routes, Route } from 'react-router-dom';
 
 function App() {
 	return (
-		<>
-			<div className='home-container'>
-				<Header />
-				<Routes>
-					<Route path='/' element={<Main />} />
-					<Route path='/about' element={<About />} />
-					<Route path='/portfolio' element={<Portfolio />} />
-					<Route path='/contact' element={<Contact />} />
-				</Routes>
-				<Footer />
-			</div>
-		</>
+		<div className='home-container'>
+			<Header />
+			<Routes>
+				<Route path='/' element={<Main />} />
+				<Route path='/about' element={<About />} />
+				<Route path='/portfolio' element={<Portfolio />} />
+				<Route path='/contact' element={<Contact />} />
+			</Routes>
+			<Footer />
+		</div>
 	);
 }
 
