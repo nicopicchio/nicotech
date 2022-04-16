f9d7151c45e48d3d3e72815b25d719757407b4ac
Nico Picchio <nicolapicchio@gmail.com>
2022-04-16 12:56:04 +0100

    about section reviewed

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -6,25 +6,23 @@ function About() {
 			<div className='about-me-container'>
 				<h3 className='about-me-title'>About</h3>
 				<p className='about-me-text'>
-					My name is Nico Picchio and I am a full stack software developer based
-					in the Midlands of the UK.
+					Hey! My name is Nico Picchio and I am a full stack software developer
+					based in the Midlands, UK.
 				</p>
 				<p className='about-me-text'>
 					I am extremely passionate about tech, problem solving and creating
 					things for the web.
 				</p>
 				<p className='about-me-text'>
-					My love with tech sparked at the end of the 80's when I was given my
-					first computer, an Apple Macintosh Plus (which I still own) and that
-					was it.
+					In the late 80's I was given my first computer, an Apple Machintosh
+					Plus, which I still proudly own and which sparked my love for
+					technology. I have been hooked ever since.
 				</p>
 				<p className='about-me-text'>
-					I started teaching myself to code in early 2020 and I was instantly
-					hooked. Writing simple lines of code suddenly becoming usable art,
-					combined with my love for technology, design and photography were good
-					enough reasons to make me realise that I wanted to leave my career in
-					Intensive Care Nursing and switch to programming. And the rest is
-					history.
+					I started teaching myself to code in 2020 and I was instantly
+					inspired by it. Writing simple lines of code which create immediately
+					usable art, combined with my love for tech, design and photography
+					were solid reasons which inspired me to be a software developer.
 				</p>
 			</div>
 			<div className='profile-image-container'>
