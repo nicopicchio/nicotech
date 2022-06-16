31e7c35952697f0d306b204b70e19f93447bc80c
Nico Picchio <nicolapicchio@gmail.com>
2022-06-16 16:24:24 +0100

    added portfolio projects and made them links

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,10 +1,48 @@
 import cubeSampleImg from '../../assets/cube-sample.jpg';
+import continentalImg from '../../assets/continental-screenshot-logo.png';
+import carbonFootprintImg from '../../assets/carbon-footprint-screenshot.png';
 
 function Portfolio() {
 	return (
 		<div id='portfolio-container'>
+			<a
+				className='portfolio-card-link'
+				href='https://thecontinentalnetwork.netlify.app/'
+				target='_blank'
+				rel='noreferrer'>
+				<div className='portfolio-card'>
+					<div>
+						<img
+							className='portfolio-card-img'
+							src={continentalImg}
+							alt='sample'
+						/>
+					</div>
+					<div className='portfolio-card-descr'>
+						<h4>The Continental Network</h4>
+					</div>
+				</div>
+			</a>
+			<a
+				className='portfolio-card-link'
+				target='_blank'
+				rel='noreferrer'
+				href='https://github.com/nicopicchio/carbon-footprint'>
+				<div className='portfolio-card'>
+					<div className='portfolio-card-img'>
+						<img
+							className='portfolio-card-img'
+							src={carbonFootprintImg}
+							alt='sample'
+						/>
+					</div>
+					<div className='portfolio-card-descr'>
+						<h4>Carbon Footprint Checker</h4>
+					</div>
+				</div>
+			</a>
 			<div className='portfolio-card'>
-				<div>
+				<div className='portfolio-card-img'>
 					<img
 						className='portfolio-card-img'
 						src={cubeSampleImg}
@@ -12,7 +50,7 @@ function Portfolio() {
 					/>
 				</div>
 				<div className='portfolio-card-descr'>
-					<h2>Lorem Ipsum</h2>
+					<h4>Lorem Ipsum</h4>
 				</div>
 			</div>
 			<div className='portfolio-card'>
@@ -24,31 +62,7 @@ function Portfolio() {
 					/>
 				</div>
 				<div className='portfolio-card-descr'>
-					<h2>Lorem Ipsum</h2>
-				</div>
-			</div>
-			<div className='portfolio-card'>
-				<div className='portfolio-card-img'>
-					<img
-						className='portfolio-card-img'
-						src={cubeSampleImg}
-						alt='sample'
-					/>
-				</div>
-				<div className='portfolio-card-descr'>
-					<h2>Lorem Ipsum</h2>
-				</div>
-			</div>
-			<div className='portfolio-card'>
-				<div className='portfolio-card-img'>
-					<img
-						className='portfolio-card-img'
-						src={cubeSampleImg}
-						alt='sample'
-					/>
-				</div>
-				<div className='portfolio-card-descr'>
-					<h2>Lorem Ipsum</h2>
+					<h4>Lorem Ipsum</h4>
 				</div>
 			</div>
 		</div>
